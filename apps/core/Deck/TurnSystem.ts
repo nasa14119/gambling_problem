36573a6dc518ffe2283416ca7efe99863c13e567
nasa14119 <nico.amaya0205@gmail.com>
2026-05-07 11:16:26 -0600

    refactor(TurnSystem): refactoring and adding comments to the code

--- apps/core/Deck/TurnSystem.ts
+++ apps/core/Deck/TurnSystem.ts
@@ -9,6 +9,7 @@ export class TurnSystem {
   manger: GameEventManager;
   turn_queue: Player[] = [];
   waiting_queue: Player[] = [];
+  players_pots: Record<string, number> = {};
   changeTurn;
   constructor(manager: GameEventManager) {
     this.manger = manager;
@@ -21,51 +22,64 @@ export class TurnSystem {
     );
   }
   async startTurn(turns: Player[]) {
-    turns.forEach((p) => {
-      p.isFold = false;
-    });
     this.turn_queue = turns;
-    let players: Record<string, number> = {};
+    this.waiting_queue = [];
     let min = -Infinity;
     while (this.turn_queue.length > 0) {
       const [current, ...rest] = this.turn_queue;
+
+      // Pop front
       this.turn_queue = rest;
+
+      // Emit turn have changed to the player that was on front
       this.changeTurn(current.playerId);
       console.log(current.playerId);
       console.log(min);
       const { type, chips } = await this.waitForEvent("player:validbet");
       if (type === "fold") {
         current.isFold = true;
-        this.turn_queue = rest;
         continue;
       }
-      // Player don't fold
-      const playerMoney = players[current.playerId] ?? 0;
-      players[current.playerId] = playerMoney + chips;
+      // From here the player whats to continue in the game
+      // Gets the current player money pot added if empty start with 0
+      const playerMoney = this.players_pots[current.playerId] ?? 0;
+      // Add the inputted chips to player money pot
+      this.players_pots[current.playerId] = playerMoney + chips;
       if (playerMoney + chips < min) {
+        // Emits error for player wrong input (This shoulde'nt happen at this point it should be catched in a earlier stage)
         this.manger.emit("player:insuficientfunds", {
-          min: min - players[current.playerId],
+          min: min - this.players_pots[current.playerId],
           player: current,
         });
         continue;
       }
+      // We know that the players current pot is greater on equal to min
       if (type === "pay") {
         this.waiting_queue = [current, ...this.waiting_queue];
         continue;
       }
       // Only raise if really new min
-      if (type === "raise" && players[current.playerId] > min) {
+      if (type === "raise" && this.players_pots[current.playerId] > min) {
         this.turn_queue.push(...this.waiting_queue);
         this.waiting_queue = [current];
-        min = players[current.playerId];
+        min = this.players_pots[current.playerId];
         continue;
       }
+
+      // This case shoulde'nt be accessible
+      // Thinking of adding a roolback system for invalid inputs
       this.turn_queue = [current, ...rest];
       this.manger.emit("player:insuficientfunds", { min, player: current });
     }
-    // Adding resulting pot to global
-    const turnPot = Object.values(players).reduce((prev, crr) => prev + crr, 0);
+    // Getting the total pot made in turn
+    const turnPot = Object.values(this.players_pots).reduce(
+      (prev, crr) => prev + crr,
+      0,
+    );
+    // Adding turn pot to the global pot
     this.moneyPot += turnPot;
+
+    // Emiting the end of the turn
     this.manger.emit("turn:end", { moneyPot: this.moneyPot });
   }
 }
