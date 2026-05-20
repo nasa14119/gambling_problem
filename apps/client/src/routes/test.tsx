7f3ebe63d023a929e05ec775031fdd69b6236f23
nasa14119 <nico.amaya0205@gmail.com>
2026-05-20 07:50:47 -0600

    routes: changing prototype poker test to route test

---
+++ apps/client/src/routes/test.tsx
@@ -0,0 +1,59 @@
+import { AdminPlayer } from '#/components/AdminPlayer'
+import { InputPlayer } from '#/components/InputPlayer'
+import { PlayerManager } from '#/components/PlayerManager'
+import { Pot } from '#/components/Pot'
+import { StartRoundBtn } from '#/components/StartRoundBtn'
+import { Table } from '#/components/Table'
+import { createFileRoute } from '@tanstack/react-router'
+import { useEffect, useState } from 'react'
+
+export const Route = createFileRoute('/test')({ component: CreateGame })
+
+function CreateGame() {
+  const [isLoading, setLoading] = useState(true)
+  useEffect(() => {
+    fetch('http://localhost:3000/api/game/new/singlePlayer', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        players: ['player:1', 'player:2', 'player:3', 'player:4', 'player:5'],
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error('Error creating game')
+        setLoading(false)
+      })
+      .catch((e) => {
+        console.error(e)
+        setLoading(false)
+      })
+  }, [])
+  if (isLoading) return <div>Loading...</div>
+  return <Game />
+}
+function Game() {
+  return (
+    <main className="size-full  h-screen relative flex justify-center items-center flex-col gap-y-5">
+      <header>
+        <Table />
+      </header>
+      <div className="flex justify-between w-full px-[10%]">
+        <PlayerManager playerId="player:1" />
+        <PlayerManager playerId="player:2" />
+      </div>
+      <div className="flex justify-between w-full px-[10%]">
+        <PlayerManager playerId="player:3" />
+        <PlayerManager playerId="player:4" />
+      </div>
+      <div>
+        <AdminPlayer playerId="player:admin" />
+      </div>
+      <StartRoundBtn />
+      <InputPlayer />
+      <Pot />
+    </main>
+  )
+}
