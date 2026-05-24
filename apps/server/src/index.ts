366705c0ede664dafa0d140cf2cd46bbdac8d3a6
nasa14119 <nico.amaya0205@gmail.com>
2026-05-24 16:09:32 -0600

    config: env vars

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,11 @@
 import cors from "cors";
 import express from "express";
 
+import env from "./env.ts";
+
+if (env.MODE === "production" && !env.SERVER_PATH) {
+  throw new Error("SERVER_PATH is required in production mode");
+}
 import { wsSever } from "./sessions/index.ts";
 import sessionRouter from "./sessions/session.routes.ts";
 const PORT = process.env.SERVER_PORT ?? 3000;
@@ -8,7 +13,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3001",
+    origin: env.SERVER_PATH,
   }),
 );
 wsSever(app);
