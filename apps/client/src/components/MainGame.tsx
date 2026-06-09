5977f0f50befcc006076bce5558e24c90ee91cbe
ppazdavi31 <ppazdavi31@gmail.com>
2026-06-08 20:19:29 -0600

    Added bg audio

--- apps/client/src/components/MainGame.tsx
+++ apps/client/src/components/MainGame.tsx
@@ -6,14 +6,21 @@ import { UserCards } from '#/components/UserCards'
 import { ExploitsSuit } from '#/exploits/ExploitsSuit'
 import { useGameEvents } from '#/hooks/useGameEvents'
 import { useGameState } from '#/stores/gameStore'
+import tableBackground from '#/assets/greenPokerTable.png'
+import { useSoundOnLoad } from '#/hooks/useSound/useSoundOnLoad'
+import music from '#/assets/soundEffects/Nivel_1_lobby.mp3'
 
 export function MainGame() {
   const game = useGameState()
+  useSoundOnLoad(music)
   useGameEvents()
   const user = game.user
   const keys = Object.keys(game.players).filter((k) => k !== user.playerId)
   return (
-    <div className="bg-green-950 rounded-4xl size-full p-2 text-white relative">
+    <div className="rounded-[100px] size-full p-2 text-white relative overflow-hidden">
+      <div className='bg-red-100 absolute inset-0'>
+        <img src={tableBackground} className='size-full object-fill [image-rendering: pixelated] '/>
+      </div>
       <ExploitsSuit />
       <div className="absolute inset-x-0 flex justify-center top-0 pt-2">
         <TableWithStore />
--- apps/client/src/hooks/useSound/index.tsx
+++ apps/client/src/hooks/useSound/index.tsx
@@ -1,22 +1,25 @@
 import { useEffect, useRef, useState } from 'react'
 
 type Options = {
-  volume: number
+  volume: number, 
+  loop: boolean
 }
 const DEFAULT_OPTIONS: Options = {
   volume: 0.5,
+  loop: false
 }
 export type AudioOptions = Partial<Options>
 export const useSound = (
   path: string,
   options: AudioOptions = {},
 ): [null, false] | [HTMLAudioElement, true] => {
-  const { volume } = { ...DEFAULT_OPTIONS, ...options }
+  const { volume, loop } = { ...DEFAULT_OPTIONS, ...options }
   const [isReady, setIsReady] = useState(false)
   const ref = useRef<HTMLAudioElement>(null)
   useEffect(() => {
     ref.current = new Audio(path)
     ref.current.volume = volume
+    ref.current.loop = loop 
     ref.current.oncanplaythrough = () => setIsReady(true)
   }, [])
   if (!ref.current || !isReady) return [null, false]
