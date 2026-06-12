f3111d26682ce3c22a4c42c190f0b83b62042fca
nasa14119 <nico.amaya0205@gmail.com>
2026-06-12 13:28:29 -0600

    feat: Layout of landing fix

--- apps/client/src/Landing/HeroLanding.tsx
+++ apps/client/src/Landing/HeroLanding.tsx
@@ -8,7 +8,7 @@ export function HeroLanding() {
       <div className="size-full grid grid-cols-2 gap-x-2">
         <aside className="flex items-center justify-end animate-in slide-in-from-bottom-10 fade-in duration-700">
           <div className="pb-[10vh]">
-            <h1 className="font-bold text-[18vh] leading-30 flex flex-col">
+            <h1 className="font-bold text-[18vh] lg:text-[140px] leading-30 flex flex-col">
               <span>Gambling</span>
               <span>Problem</span>
             </h1>
@@ -28,7 +28,7 @@ export function HeroLanding() {
             up your chances. Remember, the mafia always is dangerous; they might
             kill you if you make them mad.
           </p>
-          <Skull className="size-[80vh] absolute bottom-0 right-0 opacity-10 text-red-900" />
+          <Skull className="size-[80vh] aspect-square max-h-256 absolute bottom-0 -right-20 opacity-10 text-red-900" />
         </section>
       </div>
       <small className="absolute left-[5vh] bottom-[5vh] font-pixelated text-lg">
